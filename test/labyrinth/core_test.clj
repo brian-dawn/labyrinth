ad6b48ef933fe5876a7f0a16655d9a6b9dc4a49f
Brian Dawn <brian.dawn@code42.com>
2015-05-20 22:40:07 -0500

    Added more tests.

--- test/labyrinth/core_test.clj
+++ test/labyrinth/core_test.clj
@@ -3,8 +3,34 @@
             [labyrinth.core :refer :all]))
 
 (deftest path-tests
-  (testing "Path creation"
+  (testing "Path creation on UNIX"
     (is (= "/foo/bar" (str (path "/foo/bar"))))
     (is (= "/foo/bar" (str (path "/foo/bar/"))))
     (is (= "/foo/bar" (str (path "/foo//bar"))))
-    ))
+
+    (is (= "/foo/bar" (str (path "/foo"  "bar"))))
+    (is (= "/foo/bar" (str (path "/foo"  "bar"))))
+    (is (= "/foo/bar" (str (path "/foo/" "bar/"))))
+
+    (is (= "foo/bar" (str (path "foo"  "bar"))))
+    (is (= "foo/bar" (str (path "foo/" "bar"))))
+    (is (= "/bar"    (str (path "foo"  "/bar")))))
+
+  (testing "Path joining"
+    (is (= "foo/bar"  (str (join (path "foo")  (path "bar")))))
+    (is (= "foo/bar"  (str (join (path "foo/") (path "bar")))))
+    (is (= "/bar"     (str (join (path "foo/") (path "/bar")))))
+    (is (= "/foo/bar" (str (join (path "/foo") (path "bar"))))))
+
+  (testing "remove-root"
+    (is (= "foo" (str (remove-root (path "/foo"))))))
+
+  (testing "path-child"
+    (is (= "bar" (str (path-child (path "foo/bar")))))
+    (is (= "foo" (str (path-child (path "foo"))))))
+
+  (testing "path-parents"
+    (is (= "foo/bar" (str (path-parents (path "foo/bar/baz"))))))
+
+
+  )
